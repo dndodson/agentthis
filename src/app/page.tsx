973d20f241055d4a330e1460bb1e3c4a854f68ea
Dan Dodson <ddodson@climatecentral.org>
2024-10-08 06:52:24 -0400

    Commit landing page with hero and navbar.

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from './hero'
+import Navbar from './navbar'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <Navbar />
+      <Hero />
+    </main>
+  )
+}
